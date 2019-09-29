cd9c9f8ae8f48991df44a94d51b6075779c4f2e4
gwanderson98 <wanderson.guilherme@hotmail.com>
2019-09-29 16:43:37 -0300

    Initial commit

---
+++ app/src/main/java/com/project/appentrega1projeto/MainActivity.java
@@ -0,0 +1,45 @@
+package com.project.appentrega1projeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.users);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+
+        String spinnerLabel = adapterView.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+    }
+
+    private void displayToast(String spinnerLabel) {
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
